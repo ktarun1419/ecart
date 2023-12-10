5d9c6df5a04dbf009c5405a59e2f67e4f622352d
ktarun1419 <tarunkaushik355@gmail.com>
2023-12-10 23:58:13 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const connectBluetooth = async () => {
+    try {
+      const device = await navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+        optionalServices: ['battery_service'] // Replace with your service
+      });
+  
+      const server = await device.gatt.connect();
+      const service = await server.getPrimaryService('battery_service'); // Replace with your service
+      const characteristic = await service.getCharacteristic('battery_level'); // Replace with your characteristic
+  
+      characteristic.readValue().then(value => {
+        console.log('Battery percentage is ' + value.getUint8(0));
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
+  // Usage
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +38,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={connectBluetooth}>Connect</button>
       </header>
     </div>
   );
